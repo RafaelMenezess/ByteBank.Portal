0f8f88ac3c89b6f74c01e5169ab67e0381cecc1e
Rafael Menezes <rafa.rcm@hotmail.com>
2023-01-09 21:00:01 -0300

    Escrevendo o código de suporte http

---
+++ ByteBank.Portal/Infraestrutura/WebApplication.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace ByteBank.Portal.Infraestrutura
+{
+    public class WebApplication
+    {
+        private readonly string[] _prefixos;
+        public WebApplication(string[] prefixos)
+        {
+            if (prefixos == null)
+                throw new ArgumentNullException(nameof(prefixos));
+
+            _prefixos = prefixos;
+        }
+        public void Iniciar()
+        {
+            var httpListiner = new HttpListener();
+
+            foreach (var prefixo in _prefixos)
+            {
+                httpListiner.Prefixes.Add(prefixo);
+            }
+
+            httpListiner.Start();
+
+            var contexto = httpListiner.GetContext();
+            var requisicao = contexto.Request;
+            var resposta = contexto.Response;
+
+            var respostaConteudo = "Hello World";
+            var respostaConteudoBytes = Encoding.UTF8.GetBytes(respostaConteudo);
+
+            resposta.ContentType = "text/html; charset=utf-8";
+            resposta.StatusCode = 200;
+            resposta.ContentLength64 = respostaConteudoBytes.Length;
+
+            resposta.OutputStream.Write(respostaConteudoBytes, 0, respostaConteudoBytes.Length);
+
+            resposta.OutputStream.Close();
+
+            httpListiner.Stop();
+
+        }
+    }
+}
--- ByteBank.Portal/Program.cs
+++ ByteBank.Portal/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ByteBank.Portal.Infraestrutura;
+using System;
 
 namespace ByteBank.Portal
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            var prefixos = new string[] { "http://localhost:5341/" };
+            var webApplication = new WebApplication(prefixos);
+
+            webApplication.Iniciar();
+
+
+            Console.ReadKey();
         }
     }
 }
